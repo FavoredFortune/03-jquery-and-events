53d6c324cda29797331ec64fb0c50c0e824d6fe2
Suzanne Richman <suzanne@favoredfortune.com>
2018-02-17 23:34:00 -0800

    # Kilovolt Blog - Lab 03
    **Author**: Josh West and Suzanne Richman
    
    **Version**: 1.0.7
    
    **UPDATES** 1.0.6 and 1.0.7 was completed by Suzanne Richman alone
    
    ## Overview
    
    We want users to be able to read content easily on mobile and desktop browsers while selecting content by author or catagory, if desired and to view the blog as a multipage site (despite actually being a single page).
    
    ## Getting Started
    
    The user would need to
    * Create index.html
    * Get the icons from IcoMoon
    * Get the normalize.css from github.com/necolas/normalize.css
    * Search and find images of bacon, baseball, and cats
    * Add filler text
    * Build CSS files according to SMACSS methodology
    * Include link to jQuery library (CDN)
    * Create the articles as objects within an array with consistent property values
    * Create a constructor function to make the article array accessible to the constructor’s method
    * Use jQuery to access and clone HTML elements of the DOM and populate those elements with content from the article array
    * Create forEach loops to generate new object instances and then append them to the DOM
    * Create an array of objects to hold the content generation for the DOM for each object.
    * Create a JS file to generate a more interactive view of the DOM (selecting authors, catagories and hiding/showing full articles as well as navigating the long page as if it was a multipage website).
    
    ## Architecture
    
    We used IcoMoon icon font for navigation icons. We included the jQuery library. We used Chrome to analyze and inspect. Project is built on HTML, CSS,and JavaScript.
    
    ## Change Log
    
    02-15-2018 9:50 am - Commit to add and move readme, add normalize css, and update index.html. also updated css for improved styling.
    
    02-15-2018 10:45 am - Commit updates to articleView.js
    
    02-15-2018 12:00 pm - Commit updates to articleView.js and article.js working on filter population and handling.
    
    02-15-2018 12:53 pm - Commit updates to articleView.js and article.js working on filter population and handling and attempted nav. Only got filter population working, but it breaks when filter handler is commented in.
    
    02-15-2018 9:55 pm - Fork and clone to create my own solution to the lab, with Allie's permission given earlier today in Slack.
    
    02-17-2018  11:33 pm - Worked on and fixed layout.CSS for styling of about section. Fixed main navigation in articleView.js. Updated index.html for placement of teaser/show less. Got teaser to work, but not showing of article or show less.
    
    ## Credits and Collaborations
    
    * Thanks to our instructors and TAs.
    * We referenced the jQuery cheat sheet: https://oscarotero.com/jquery/
    * We used a normalize.css file found here: github.com/necolas/normalize.css
    * We referenced our text book: Jon Duckett - JavaScript and JQuery.ç
    /q

--- josh-sooz/scripts/articleView.js
+++ josh-sooz/scripts/articleView.js
@@ -13,7 +13,7 @@ articleView.populateFilters = function() {
       // To do so, Build an <option> DOM element that we can append to the author <select> element.
       // Start by grabbing the author's name from `this` article element, and then use that bit of text to create the option tag (in a variable named `optionTag`) that we can append to the #author-filter select element.
       authorName = $(this).attr('data-author');
-      console.log(authorName);
+      // console.log(authorName);
 
       // DONE: Refactor this concatenation using a template literal.
       optionTag = `<option value="${authorName}">${authorName}</option>`;
@@ -49,8 +49,9 @@ articleView.handleAuthorFilter = function() {
     else {
       // DONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
       $('article').fadeIn(FadeIn_TimeOut);
+      $('article.template').hide();
     }
-    $('#author-filter').val('');
+    $('#category-filter').val('');
   });
 };
 
@@ -68,26 +69,20 @@ articleView.handleCategoryFilter = function() {
     else {
       // DONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
       $('article').fadeIn(FadeIn_TimeOut);
+      $('article.template').hide();
     }
-    $('#category-filter').val('');
+    $('#author-filter').val('');
   });
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  // DONE: Add an event handler to .main-nav elements that will power the Tabs feature.
 
-  //GOT THIS TO STOP BREAKING NAV, BUT DOESN"T WORK CORRECTLY
-
-  $('.main-nav').on('click', '.tab', function(){
-    if($(this).val()){
+  $('.main-nav').on('click', '.tab', function(e){
     //hide all tab content when clicked on
-      $('.tab').hide();
-      $(`#${$(this).data('tab')}`).fadeIn(FadeIn_TimeOut);
-    } else{
-      $('.tab').fadeIn(FadeIn_TimeOut);
-    }
-    $('.tab').val('');
-  })
+    $('.tab-content').hide();
+    $(`#${$(this).data('content')}`).fadeIn(FadeIn_TimeOut);
+  });
 
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
@@ -100,16 +95,21 @@ articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
 
-  // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  // DONE: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 
-  //GOT THIS TO HIDE, BUT NOT SHOW
-  $('article').on('click','a.read-on', function (){
-    if(($(this).text() === 'Read on &rarr;')){
+  $('article').on('click','a .read-on', function(e){
+    if($(this).text() === 'Read on →'){
       $(this).parent().find('*').fadeIn(FadeIn_TimeOut);
+      $(this).html('Show less &larr;');
+    } else{
+      $('body').fadeIn(FadeIn_TimeOut);
+      $(this).html('Read on &rarr;');
+      $(this).text('Read on &rarr;');
+      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
     }
   });
-}
+};
 
 // DONE: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function(){
